745901e3cdc218ee8d1d69ec5b810cfe4c989511
codeif <me@codeif.com>
2020-04-06 03:33:23 +0800

    get_object方法

--- qcos/client.py
+++ qcos/client.py
@@ -6,9 +6,8 @@ from .auth import CosS3Auth
 
 
 class Client(object):
-
-    def __init__(self, secret_id, secret_key, region, bucket, scheme='https'):
-        assert scheme in ['http', 'https']
+    def __init__(self, secret_id, secret_key, region, bucket, scheme="https"):
+        assert scheme in ["http", "https"]
 
         self.secret_id = secret_id
         self.secret_key = secret_key
@@ -20,23 +19,25 @@ class Client(object):
         self.session = requests.Session()
 
     def get_url(self, key):
-        return urljoin(f'{self.scheme}://{self.bucket}.cos.{self.region}.myqcloud.com', key)
+        return urljoin(
+            f"{self.scheme}://{self.bucket}.cos.{self.region}.myqcloud.com", key
+        )
 
     def request(self, method, key, **kwargs):
         url = self.get_url(key)
         return self.session.request(
-            method,
-            url,
-            auth=CosS3Auth(self.secret_id, self.secret_key, key),
-            **kwargs,
+            method, url, auth=CosS3Auth(self.secret_id, self.secret_key, key), **kwargs,
         )
 
     def head_object(self, key):
-        return self.request('HEAD', key)
+        return self.request("HEAD", key)
+
+    def get_object(self, key):
+        return self.request("GET", key)
 
     def put_object(self, key, data, **kwargs):
-        return self.request('PUT', key, data=data, **kwargs)
+        return self.request("PUT", key, data=data, **kwargs)
 
     def put_local(self, key, local_path, **kwargs):
-        with open(local_path, 'rb') as f:
-            return self.request('PUT', key, data=f, **kwargs)
+        with open(local_path, "rb") as f:
+            return self.request("PUT", key, data=f, **kwargs)
--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 
 setup(
-    name='qcos',
-    version='2.0.0',
-    description='腾讯云对象存储库，支持命令行',
+    name="qcos",
+    version="2.0.1",
+    description="腾讯云对象存储库，支持命令行",
     long_description=readme,
-    author='codeif',
-    author_email='me@codeif.com',
-    url='https://github.com/codeif/qcos',
-    license='MIT',
-    entry_points={
-        'console_scripts': [
-            'qcos = qcos.cli:main',
-        ],
-    },
-    install_requires=['requests', 'termcolor'],
+    author="codeif",
+    author_email="me@codeif.com",
+    url="https://github.com/codeif/qcos",
+    license="MIT",
+    entry_points={"console_scripts": ["qcos = qcos.cli:main"]},
+    install_requires=["requests", "termcolor"],
     packages=find_packages(exclude=("tests", "tests.*")),
 )
